12c7c9039e7b5ab9b29ee2e92235831218e27160
KarenSRG <69356471+KarenSRG@users.noreply.github.com>
2025-02-28 15:44:49 +0400

    Update task1.cpp

--- feb20/task1.cpp
+++ feb20/task1.cpp
@@ -1,30 +1,37 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
 
 class ArrayHandler {
 private:
-    vector<double> arr;
+    double* arr_ptr;
     int size;
 
 public:
-    ArrayHandler(double* arr_ptr, int s) {
+    ArrayHandler(double* arr, int s) {
         size = s;
-        arr.assign(arr_ptr, arr_ptr + s); 
+        arr_ptr = new double[size]; 
+        for (int i = 0; i < size; i++) {
+            arr_ptr[i] = arr[i];
+        }
     }
 
     ArrayHandler(const ArrayHandler& other) {
         size = other.size;
-        arr = other.arr;
+        arr_ptr = new double[size];
+        for (int i = 0; i < size; i++) {
+            arr_ptr[i] = other.arr_ptr[i];
+        }
+    }
+    ~ArrayHandler() {
+        delete[] arr_ptr;
     }
 
     double midNegative() {
         double sum = 0;
         int count = 0;
-        for (double num : arr) {
-            if (num < 0) {
-                sum += num;
+        for (int i = 0; i < size; i++) {
+            if (arr_ptr[i] < 0) {
+                sum += arr_ptr[i];
                 count++;
             }
         }
@@ -37,9 +44,9 @@ public:
 void printGreaterThanMidNegative(ArrayHandler arr) {
     double mid_neg = arr.midNegative();
     cout << "Elements greater than mid_negative (" << mid_neg << "): ";
-    for (double num : arr.arr) {
-        if (num > mid_neg) {
-            cout << num << " ";
+    for (int i = 0; i < arr.size; i++) {
+        if (arr.arr_ptr[i] > mid_neg) {
+            cout << arr.arr_ptr[i] << " ";
         }
     }
     cout << endl;
@@ -49,13 +56,13 @@ int main() {
     int n;
     cout << "Enter size of array: ";
     cin >> n;
-
+    
     double arr[n];
     cout << "Enter " << n << " elements: ";
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-
+    
     ArrayHandler handler(arr, n);
     printGreaterThanMidNegative(handler);
 
